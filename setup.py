f88660306fc33213bd2188040bb03be6c3c43dd6
YannThorimbert <yann.thorimbert@gmail.com>
2017-01-07 02:29:38 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(name='thorpy',
+      packages = ['thorpy'],
       version='1.5.3',
       description='pygame GUI library',
       author='Yann Thorimbert',
       author_email='yann.thorimbert@gmail.com',
       url='http://www.thorpy.org/',
+      download_url='https://github.com/YannThorimbert/Thorpy/tarball/0.1',
+      keywords=['pygame','gui','menus','buttons'],
       packages=find_packages(),
       include_package_data=True,
       license='MIT')
