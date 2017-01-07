c23c9c58c53e2832d8a7042403e55bf51b64ef86
YannThorimbert <yann.thorimbert@gmail.com>
2017-01-07 02:25:31 +0100

    added files for pypi submission

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(name='thorpy',
+      version='1.5.3',
+      description='pygame GUI library',
+      author='Yann Thorimbert',
+      author_email='yann.thorimbert@gmail.com',
+      url='http://www.thorpy.org/',
+      packages=find_packages(),
+      include_package_data=True,
+      license='MIT')
