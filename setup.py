aac24b41757e7c2e4b94d03a0cc599acab583052
YannThorimbert <yann.thorimbert@gmail.com>
2017-02-10 13:04:19 +0100

    cleaned version name

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(name='thorpy',
-      version='1.5.3',
+      version='1.5.4',
       description='pygame GUI library',
       author='Yann Thorimbert',
       author_email='yann.thorimbert@gmail.com',
       url='http://www.thorpy.org/',
-##      download_url='https://github.com/YannThorimbert/Thorpy/tarball/1.5.3',
       keywords=['pygame','gui','menus','buttons'],
       packages=find_packages(),
       include_package_data=True,
