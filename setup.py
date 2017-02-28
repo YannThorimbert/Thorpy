d44108a353f36324724bf315dd22af525d18360b
YannThorimbert <yann.thorimbert@gmail.com>
2017-03-01 00:47:53 +0100

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(name='thorpy',
-      version='1.5.4',
+      version='1.5.5',
       description='pygame GUI library',
       author='Yann Thorimbert',
       author_email='yann.thorimbert@gmail.com',
       url='http://www.thorpy.org/',
-      keywords=['pygame','gui','menus','buttons'],
+      keywords=['pygame','gui','menus','buttons', 'widgets'],
       packages=find_packages(),
       include_package_data=True,
       license='MIT')
