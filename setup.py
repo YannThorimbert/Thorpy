5c8bcaa9ec4ca17b90d8d3f70897f2a0a1300060
YannThorimbert <yann.thorimbert@gmail.com>
2017-03-27 00:11:57 +0200

    changed version number in setup.py to 1.5.7

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import find_packages, setup
 
-print("PACKAGES!!!",find_packages())
 
 setup(name='thorpy',
-      version='1.5.6a1',
+      version='1.5.7',
       description='pygame GUI library',
       author='Yann Thorimbert',
       author_email='yann.thorimbert@gmail.com',
